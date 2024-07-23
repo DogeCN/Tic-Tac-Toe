631f188fa9c0ba93df020a4df203fb0b883916f2
DogeCN <hark2009lbf@outlook.com>
2024-07-23 21:01:39 +0800

    Add Button Identifier

--- warpper.py
+++ warpper.py
@@ -5,6 +5,7 @@ class Button(QPushButton):
     style_sheet = ''
     color = (255, 255, 255)
     opacity = 255
+    belong = None #type: Wrapper
 
     def __init__(self, parent, row, column):
         super().__init__(parent)
@@ -12,15 +13,15 @@ class Button(QPushButton):
         self.setFlat(True)
         self.row = row
         self.column = column
-    
+
     def style_update(self):
         color = f'color: rgba{(*self.color, self.opacity)};'
         self.setStyleSheet(self.style_sheet + color)
 
     def react(self):
-        WapperInstances.default.apply(self)
+        WrapperInstances.default.apply(self)
 
-class ButtonStack(list[Button]):
+class ButtonQueue(list[Button]):
     def __init__(self):
         super().__init__()
     
@@ -30,12 +31,13 @@ class ButtonStack(list[Button]):
             button = self[i]
             opacity = 255 - i*255//3
             if not opacity:
-                WapperInstances.empty.apply(self.pop())
+                WrapperInstances.empty.apply(self.pop())
             else:
                 button.opacity = opacity
                 button.style_update()
 
-class RawWapper:
+class RawWrapper:
+    name = ''
     text = ''
     icon = None #type: QIcon
     style = 'font-size: 40px;'
@@ -49,18 +51,24 @@ class RawWapper:
         button.style_sheet = self.style
         button.style_update()
 
-class Warpper(RawWapper):
+class Wrapper(RawWrapper):
 
-    def __init__(self):
-        self.stack = ButtonStack()
+    def __init__(self, index):
+        self.index = index
+        self.queue = ButtonQueue()
 
     def apply(self, button:Button):
         super().apply(button)
-        self.stack.append(button)
+        self.queue.append(button)
+        button.belong = self
 
-class WapperGroup:
-    wappers = (Warpper(), Warpper())
+class WrapperGroup:
+    wrappers = (Wrapper(0), Wrapper(1))
     _index = 0
+
+    def __init__(self):
+        self.wrapper(self.index).name = 'First Player'
+        self.wrapper(self.index).name = 'Second Player'
     
     @property
     def index(self):
@@ -69,29 +77,29 @@ class WapperGroup:
         return index
 
     @property
-    def stack(self):
-        return self.wapper(0).stack + self.wapper(1).stack
+    def queue(self):
+        return self.wrapper(0).queue + self.wrapper(1).queue
 
-    def wapper(self, index:int):
-        return self.wappers[index]
+    def wrapper(self, index:int):
+        return self.wrappers[index]
 
     def apply(self, button:Button):
-        if button not in self.stack:
-            wapper = self.wappers[self.index]
-            wapper.apply(button)
+        if button not in self.queue:
+            wrapper = self.wrappers[self.index]
+            wrapper.apply(button)
 
-class WapperInstances:
+class WrapperInstances:
 
-    default = WapperGroup()
+    default = WrapperGroup()
 
-    wapper1 = default.wapper(0)
-    wapper1.text = '〇'
-    wapper1.style = 'font: 700 60px;'
-    wapper1.color = (0, 255, 0)
+    wrapper1 = default.wrapper(0)
+    wrapper1.text = '〇'
+    wrapper1.style = 'font: 700 60px;'
+    wrapper1.color = (0, 255, 0)
 
-    wapper2 = default.wapper(1)
-    wapper2.text = '⨯'
-    wapper2.style = 'font-size: 80px;'
-    wapper2.color = (255, 0, 0)
+    wrapper2 = default.wrapper(1)
+    wrapper2.text = '⨯'
+    wrapper2.style = 'font-size: 80px;'
+    wrapper2.color = (255, 0, 0)
 
-    empty = RawWapper()
+    empty = RawWrapper()
